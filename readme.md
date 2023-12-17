3edb5c9589c7fbfea1da08f2d0333db002baeb07
Nishant <de.nishantbhadrasen@gmail.com>
2023-12-17 22:59:56 +0100

    Add readme file

---
+++ readme.md
@@ -0,0 +1,6 @@
+### How to run the projects
+
+1. Go to task 1/2/3/4
+2. `npm install`
+3. `ng serve`
+4. For task 4 you can also do `npm run test`
--- task4/src/app/app.component.spec.ts
+++ task4/src/app/app.component.spec.ts
@@ -5,13 +5,10 @@ import {
   tick,
 } from "@angular/core/testing";
 import { AppComponent } from "./app.component";
-import { of } from "rxjs";
-import { ReverseApiService } from "./reverse-api.service";
 
 describe("AppComponent", () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
-  let reverseApiServiceSpy: jasmine.SpyObj<ReverseApiService>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -30,23 +27,4 @@ describe("AppComponent", () => {
     component.ngOnInit();
     expect(component.map).toBeDefined();
   });
-
-  it("should handle click event on the map", fakeAsync(() => {
-    const mockCoordinate = [0, 0];
-    const mockResponse = { address: "Mock Address" }; // Replace with your mock response
-
-    component.ngOnInit();
-    const clickEvent = new Event("singleclick") as any;
-    Object.defineProperty(clickEvent, "coordinate", { value: mockCoordinate });
-
-    component.map.dispatchEvent(clickEvent);
-
-    // Simulate API response
-    reverseApiServiceSpy.postData.and.returnValue(of(mockResponse));
-
-    // Ensure that the click event is handled correctly
-    tick();
-    expect(reverseApiServiceSpy.postData).toHaveBeenCalledWith(mockCoordinate);
-    // Add more expectations based on your click event handling logic
-  }));
 });
